901aaafd99a17408e49d0161b42ac93edbb945db
kriangkrai <st651102064101@pcru.ac.th>
2025-12-27 02:39:47 +0700

    Enable watching for changes in ecosystem config and update skip prefixes in server.js for additional routes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,8 @@ module.exports = {
       script: 'server.js',
       cwd: '/Users/kriangkrai/pcru-chatbot-backend-1',
       watch: false,
+      watch: true,
+      ignore_watch: ['node_modules', 'logs', '.git', 'uploads'],
       env: {
         NODE_ENV: 'production'
       }
